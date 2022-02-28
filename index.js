0b2e8b0e8b8217c837b82c04416964a0f95c8d79
Boby Vendramin <robertson.vendramin@gmail.com>
2022-02-28 11:36:38 -0300

    Adicionando velocidade o player e loop da animação

--- index.js
+++ index.js
@@ -11,6 +11,10 @@ class Player {
             x: 100, 
             y: 100
         }
+        this.velocity = {
+            x: 0,
+            y: 1
+        }
         this.width = 30
         this.height = 30
     }
@@ -18,7 +22,19 @@ class Player {
         c.fillStyle = 'red'
         c.fillRect(this.position.x, this.position.y, this.width, this.height)
     }
+
+    update() {
+        this.position.y += this.velocity.y
+        this.draw()
+    }
 }
 
 const player = new Player()
-player.draw()
+player.update()
+
+function animate() {
+    requestAnimationFrame(animate)
+    console.log('go')
+}
+
+animate()
